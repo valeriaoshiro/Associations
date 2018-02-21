0dd3a792041994d56bcfd7b67604d5dcbd67e7f8
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-02-20 16:10:11 -0800

    Implemented reference example in db association

---
+++ reference.js
@@ -0,0 +1,80 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/blog_demo2');
+
+// USER - email, name
+// POST - title, content
+
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+var Post = mongoose.model('Post', postSchema);
+
+var userSchema = new mongoose.Schema({
+   email: String,
+   name: String,
+   posts: [
+       {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post"
+        }
+    ]
+});
+var User = mongoose.model('User', userSchema);
+
+
+
+// var newUser = new User({
+//     email: 'hermione@howards.edu',
+//     name: 'Hermione Granger'
+// });
+// newUser.save(function(err, user){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(user);
+//     }
+// });
+// var newPost = new Post({
+//     title: 'Reflections on Apples',
+//     content: 'They are delicious'
+// });
+// newPost.save(function(err, post){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+// //another way of creating a post
+// Post.create({
+//     title: "Blog 2",
+//     content: "This is the test blog"
+// }, function(err, post){
+//     User.findOne({email: "hermione@howards.edu"}, function(err, foundUser){
+//         if(err){
+//             console.log(err)
+//         } else {
+//             foundUser.posts.push(post._id);
+//             foundUser.save(function(err, data){
+//                 if(err){
+//                     console.log(err);
+//                 } else {
+//                     console.log(data);
+//                 }
+//             });
+//         }
+//     });
+// });
+
+//find user
+//find all posts for that user
+
+User.findOne({email: 'hermione@howards.edu'}).populate("posts").exec(function(err, user){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(user);
+    }
+});
+
